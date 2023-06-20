07be8a2c104f8cea3a59450ad8a31a302a82d262
Kyuseok Park <dosim0106@gmail.com>
2023-06-21 03:43:59 +0900

    FEAT: Searching by Initial hangul

--- SearchingPokemons.jsx
+++ SearchingPokemons.jsx
@@ -2,10 +2,22 @@ import React, { useMemo, useRef, useState } from 'react';
 import { css } from '@emotion/react';
 import PokemonData from './PokemonData';
 
+const INITIAL_HANGUL = [
+  'ㄱ', 'ㄲ', 'ㄴ', 'ㄷ', 'ㄸ',
+  'ㄹ', 'ㅁ', 'ㅂ', 'ㅃ', 'ㅅ',
+  'ㅆ', 'ㅇ', 'ㅈ', 'ㅉ', 'ㅊ',
+  'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ',
+];
+const FIRST_FULL_HANGUL_CODE = '가'.charCodeAt();
+const LAST_FULL_HANGUL_CODE = '힣'.charCodeAt();
+const FIRST_INITIAL_HANGUL_CODE = 'ㄱ'.charCodeAt();
+const LAST_INITIAL_HANGUL_CODE = 'ㅎ'.charCodeAt();
+
 const matchedDataStyle = css({
   border: '1px solid black',
   width: '150px',
 });
+
 
 const makeCustomData = () => {
   const nameToId = {};
@@ -17,6 +29,12 @@ const makeCustomData = () => {
   return [nameToId, names];
 }
 
+const getInitialByFullHangul = (fullHangul) => {
+  if (fullHangul) {
+    return INITIAL_HANGUL[Math.floor((fullHangul.charCodeAt() - '가'.charCodeAt()) / 588)];
+  }
+}
+
 const SearchingPokemons = () => {
   const [nameToId, names] = useMemo(makeCustomData, []);
 
@@ -26,6 +44,7 @@ const SearchingPokemons = () => {
 
   const onClickButton = (e) => {
     e.preventDefault();
+    setMatchedDataList([]);
     const searchedId = nameToId[inputValue];
     if (searchedId) {
       setImgURL(`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${searchedId}.png`);
@@ -38,15 +57,32 @@ const SearchingPokemons = () => {
   const onChangeInput = (e) => {
     const value = e.target.value;
     setInputValue(value);
+    const valueCode = value.charCodeAt();
     setMatchedDataList(names.filter(name => {
-      let flag = value[0] === name[0];
-      for (let i = 1; i < value.length; i++) {
-        flag = flag && (value[i] === name[i]);
+      if (FIRST_FULL_HANGUL_CODE <= valueCode && valueCode <= LAST_FULL_HANGUL_CODE) {
+        return isMatchedDataByFullHangul(name, value);
+      } else if (FIRST_INITIAL_HANGUL_CODE <= valueCode && valueCode <= LAST_INITIAL_HANGUL_CODE) {
+        return isMatchedDataByInitialHangul(name, value);
+      } else {
+        return false;
       }
-      return flag;
     }));
+  }
 
-    console.log(matchedDataList);
+  const isMatchedDataByFullHangul = (name, value) => {
+    let flag = value[0] === name[0];
+    for (let i = 1; i < value.length; i++) {
+      flag = flag && (value[i] === name[i]);
+    }
+    return flag;
+  }
+
+  const isMatchedDataByInitialHangul = (name, value) => {
+    let flag = value[0] === getInitialByFullHangul(name[0]);
+    for (let i = 1; i < value.length; i++) {
+      flag = flag && (value[i] === getInitialByFullHangul(name[i]));
+    }
+    return flag;
   }
 
   return (
